38b31aeaf8a630d5bb59bc237081b8a8917b650f
ritweekraj288 <ritweekraj288@gmail.com>
2025-09-06 00:22:05 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,75 @@
+var buttonColours=["red","blue","green","yellow"];
+
+var gamePattern=[];
+var userClickedPattern=[];
+
+var started=false;
+var level=0;
+
+$(document).keypress(function(){
+    if(!started){
+        $("h1").html("Level "+level);
+        nextSequence();
+        started=true;
+    }
+});
+
+$(".btn").click(function(){
+        var userChosenColour=$(this).attr("id");
+        userClickedPattern.push(userChosenColour);
+        
+        playSound(userChosenColour);
+        animatePress(userChosenColour);
+
+        checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+        if(gamePattern.length===userClickedPattern.length){
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }else{
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+                $("body").removeClass("game-over");;
+            }, 200);
+        $("h1").html("Game Over, Press Any Key to Restart"); 
+        startOver();   
+    }
+}
+
+function nextSequence(){
+    
+    level++;
+    $("h1").html("Level "+level);
+    var randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColour=buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio=new Audio("./sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+            $("#"+currentColour).removeClass("pressed");
+        },100);
+
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
+}
